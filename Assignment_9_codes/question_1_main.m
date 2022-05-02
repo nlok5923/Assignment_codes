19367337d2d871b04caf0d2ff08b2ab0bfecc476
Nitanshu Lokhande <56120084+nlok5923@users.noreply.github.com>
2022-05-02 10:34:17 +0530

    minor updates in code

--- Assignment_9_codes/question_1_main.m
+++ Assignment_9_codes/question_1_main.m
@@ -1,31 +1,3 @@
-// Q_function= zeros(1,10); 
-// N = zeros(1,10);
-// Rn = zeros(1,1000);
-// iterate = zeros(1,1000);
-
-// alpha = 0.2;
-// epsilon=0.1;
-// sum = 0;
-
-// for idx = 1:1000
-//     iterate(idx) = idx;
-//     if rand > epsilon
-//         [m , id] = max(Q_function);
-//         A = id;
-//     else
-//         temp = randperm(10); 
-//         A = temp(1);
-//     end
-//     R = bandit_non_stationary(A);
-//     N(A) = N(A) + 1;
-//     Q_function(A) = Q_function(A) + alpha * (R - Q_function(A));
-//     sum = sum + R;
-//     Rn(idx) = sum/idx;
-// end
-
-// plot(iterate,Rn);
-
-
 Q_function= zeros(1,2); 
 N= zeros(1,2);
 Rn= zeros(1,1000);
@@ -48,11 +20,9 @@ for iter = 1:1000
     end
     % R = binaryBanditB(A)
     % R = binaryBanditA(A)
-    % A
     N(A)= N(A)+1;
     Q_function(A)= Q_function(A)+ alpha*(R - Q_function(A));
     sum = sum + R;
-    % sum = sum / iter
     Rn(iter) = sum / iter;
 end
 plot(itr,Rn);
